59bb92ba695969e7f2dde51bcb6715d387620ada
Oleksandra-lab <127051632+Oleksandra-lab@users.noreply.github.com>
2023-10-12 10:55:50 +0300

    filter updated

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { FilterWrap } from './Filter.styled';
 
-export default function Filter({ handleFilterChange }) {
+export default function Filter({ onFilter }) {
   return (
     <FilterWrap>
       Find contact by name
       <input
         type="text"
         name="filter"
-        onChange={evt => handleFilterChange(evt.target.value)}
+        onChange={evt => onFilter(evt.target.value)}
       />
     </FilterWrap>
   );
